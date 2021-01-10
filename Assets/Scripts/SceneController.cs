7753892fc6755abf6bbacf33a27d16c9b30676db
ZombieLoc0 <ZombieLoc0@gmail.com>
2021-01-10 10:52:25 -0600

    Mecanicas Base

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject loseCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void lose()
+    {
+        loseCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
